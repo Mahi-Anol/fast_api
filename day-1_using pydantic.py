7967575bf20c3aaedddf556d6f9e89ddcc092f7f
Mahi Sarwar Anol <91027428+Mahi-Anol@users.noreply.github.com>
2024-03-30 14:22:06 +0600

    Update day-1_using pydantic.py

--- day-1_using pydantic.py
+++ day-1_using pydantic.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -8,6 +9,8 @@ app=FastAPI()
 class post(BaseModel):
     title: str
     massage: str
+    published: bool=True
+    rating: Optional[int]=None
 
 @app.get("/")
 async def read_root():
@@ -15,5 +18,5 @@ async def read_root():
 
 @app.post("/mass")
 async def post_m(new_post: post):
-    print(new_post)
+    print(new_post.dict())
     return {"Your post was":f"{new_post.title}, {new_post.massage}"}
