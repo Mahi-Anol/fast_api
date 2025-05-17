663d805f34f52c576324bf45ca1b6d29511a5055
Mahi Sarwar Anol <91027428+Mahi-Anol@users.noreply.github.com>
2025-05-17 11:53:11 +0600

    Add files via upload

---
+++ user_data_retirve_with_get_request.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import json
+
+
+app=FastAPI()
+
+
+def json_file_loader():
+    with open('D:\Fast-api\patients.json','r') as f:
+        data=json.load(f)
+    return data
+@app.get("/get_all_users_data")
+def get_all_user_data():
+
+    data=json_file_loader()
+    return data
+
+@app.get("/get_specific_user_data")
+def get_specific_user_data(user_id):
+
+    data=json_file_loader()
+    return data[user_id]
