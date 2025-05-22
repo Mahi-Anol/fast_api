d6bb1097209857a74a283bb0a11a82ed194eb83b
Mahi Sarwar Anol <anol.mahi@gmail.com>
2025-05-22 13:45:42 +0600

    a

--- pydentic.py
+++ pydentic.py
@@ -1,14 +1,18 @@
-from pydantic import BaseModel
-from typing import List,Dict
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List,Dict,Optional,Annotated
+
+from decimal import Decimal
 
 class patient(BaseModel):
-    name: str
+    name: Annotated[str,Field(max_length=50,title='Name of the patient',description="name of the patient in 50 charecters",examples=['Nititsh','Amit'],default='MEIN')]
     age: int
-    weight:float
-    maried:bool
-    allergies:List[str]
+    email:EmailStr
+    phone_no:Decimal=Field(max_digits=11)
+    url:AnyUrl
+    weight:Annotated[float,Field(gt=0,strict=True)]
+    maried:bool = False ## Cannot assign None when creating object for this one.
+    allergies:Annotated[Optional[List[str]],Field(max_length=4,default=None)] ## Can assign None when needed.
     contact_detail:Dict[str,str]
-
 
 # def insert_patient_without_pydantic(name,age):
 #     print(name)
@@ -18,13 +22,16 @@ class patient(BaseModel):
 def insert_patient_with_pydantic(patient:patient):
     print(patient.name)
     print(patient.age)
+    print(patient.email)
+    print(patient.phone_no)
+    print(patient.url)
     print(patient.weight)
     print(patient.maried)
     print(patient.allergies)
     print(patient.contact_detail)
     print('Inserted')
 
-patient_info={'name':'mahi','age': 30,'weight':70.6,'maried':True,'allergies':['a','b','c'],'contact_detail':{'phone':'018167035255','email':'fff'}}
+patient_info={'name':'mahi','email':'aba@mail.com','phone_no':16700763,'url':'https://www.mahi.com.uk','age': 30,'weight':2,'allergies':None,'maried':True,'contact_detail':{'phone':'018167035255','email':'fff'}}
 
 patient1=patient(**patient_info)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-def insert_patient_with_pydantic(patient:patient):
-    print(patient.name)
-    print(patient.age)
-    print(patient.weight)
-    print(patient.married)
-    print(patient.allergies)
-    print(patient.contact_detail)
-    print('Inserted')
+'name':'mahi'
