b6c6e73ea2309fe65e50fbc6f7fbc601e80ac80b
Mahi Sarwar Anol <anol.mahi@gmail.com>
2025-05-22 14:24:20 +0600

    a

--- pydantic_field_validator.py
+++ pydantic_field_validator.py
@@ -1,13 +1,36 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,field_validator
 from typing import List,Dict
 
 class patient(BaseModel):
     name: str
     age: int
+    email:str
     weight:float
     maried:bool
     allergies:List[str]
     contact_detail:Dict[str,str]
+
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        valid_domain=['hdfc.com','icic.com']
+        domain_name=value.split('@')[-1]
+        if domain_name not in valid_domain:
+            raise ValueError('Not a valid domain')
+        else:
+            return value
+    @field_validator('name')
+    @classmethod
+    def transform_name(cls,value):
+        return value.upper()
+    
+    @field_validator('age',mode='after') ###when mode set to before it wont woork because in that case it works before type casting.
+    @classmethod
+    def age_validator(cls,value):
+        if 0 <value<50:
+            return value
+        else:
+            raise ValueError('value of age should be between 0 to 50')
 
 
 # def insert_patient_without_pydantic(name,age):
@@ -18,13 +41,14 @@ class patient(BaseModel):
 def insert_patient_with_pydantic(patient:patient):
     print(patient.name)
     print(patient.age)
+    print(patient.email)
     print(patient.weight)
     print(patient.maried)
     print(patient.allergies)
     print(patient.contact_detail)
     print('Inserted')
 
-patient_info={'name':'mahi','age': 30,'weight':70.6,'maried':True,'allergies':['a','b','c'],'contact_detail':{'phone':'018167035255','email':'fff'}}
+patient_info={'name':'mahi','email':"mahi@icic.com",'age': '30','weight':70.6,'maried':True,'allergies':['a','b','c'],'contact_detail':{'phone':'018167035255','email':'fff'}}
 
 patient1=patient(**patient_info)
 
